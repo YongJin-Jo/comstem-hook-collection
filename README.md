897093717a61b7258d45005d79ecff216ff334f4
YongJin-Jo <artinc5233@gmail.com>
2022-01-20 21:31:46 +0900

    커스텀 훅 useInput 구현

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-"# comstem-hook-collection" 
+# comstem hook을 정리한 라이브러리입니다.
+
+## useInput
+```tsx
+// 선언 방법
+const input = useInput<T>("Yongin-Jo",callback);
+```
+useInput은 state와 onchange를 반환하는 함수입니다. 
+첫번째 인자는 state에 들어갈 초기값을 넣어줍니다. 타입은 string 또는 number입니다.
+두번재 인자는 callback 함수를 넣어줍니다. 제네릭 타입을 정의 해주셔야 합니다.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-export {};
+import React, { ChangeEvent, useState } from 'react';
+
+/**
+ * useInput 은 input 태그의 상태의 제어를 쉬게 해주는 함수입니다.
+ *  @retuurn  {value,onChange}
+ * */
+const useInput = <callback>(
+  initialValue: string | number,
+  validator: callback
+) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    let willUdate = true;
+    if (typeof validator === 'function') {
+      willUdate = validator(value);
+      console.log(willUdate);
+    }
+    if (willUdate) {
+      const targetValue = value as any;
+      setValue(targetValue);
+    }
+  };
+  return { value, onChange };
+};
+
+export { useInput };
